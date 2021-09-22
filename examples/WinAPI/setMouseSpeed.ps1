cac747f5dec99fa4a3faeabde878f4ade7767a4f
alfish2000 <alfish2000@gmail.com>
2021-09-22 14:13:24 +0200

    Add mouse speed toggle

--- examples/WinAPI/setMouseSpeed.ps1
+++ examples/WinAPI/setMouseSpeed.ps1
@@ -5,6 +5,18 @@ param (
 
 set-strictMode -version latest
 
+# Toggle mouse speed between 10 and 20 when no parameter is given
+if ($newSpeed -eq 0)
+{
+    $currentSpeed = (get-itemProperty 'hkcu:\Control Panel\Mouse').MouseSensitivity
+    switch ( $currentSpeed )
+    {
+        10 { $newSpeed = 20 }
+        20 { $newSpeed = 10 }
+        default { $newSpeed = 10 }
+    }
+}
+
 $winApi = add-type -name user32 -namespace tq84 -passThru -memberDefinition '
    [DllImport("user32.dll")]
     public static extern bool SystemParametersInfo(
@@ -17,14 +29,13 @@ $winApi = add-type -name user32 -namespace tq84 -passThru -memberDefinition '
 
 $SPI_SETMOUSESPEED = 0x0071
 
-"MouseSensitivity before WinAPI call:  $((get-itemProperty 'hkcu:\Control Panel\Mouse').MouseSensitivity)"
-
 $null = $winApi::SystemParametersInfo($SPI_SETMOUSESPEED, 0, $newSpeed, 0)
 
 #
 #    Calling SystemParametersInfo() does not permanently store the modification
 #    of the mouse speed. It needs to be changed in the registry as well
 #
-"MouseSensitivity after WinAPI call:  $((get-itemProperty 'hkcu:\Control Panel\Mouse').MouseSensitivity)"
 
 set-itemProperty 'hkcu:\Control Panel\Mouse' -name MouseSensitivity -value $newSpeed
+
+"MouseSensitivity set to: $((get-itemProperty 'hkcu:\Control Panel\Mouse').MouseSensitivity)"
